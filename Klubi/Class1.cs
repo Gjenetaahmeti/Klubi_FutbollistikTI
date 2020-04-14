05e2ebc927f5ace1aa233720b15d27f0f1a4e2c7
alttinl <altin.loshi@riinvest.net>
2020-04-14 23:16:19 +0200

    Krijimi i calss library.dal

---
+++ Klubi/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Klubi
+{
+    public class Class1
+    {
+    }
+}
---
+++ Klubi_I_Futbollit.DAL/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Klubi_I_Futbollit.DAL")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Klubi_I_Futbollit.DAL")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8c99e032-cb6a-4c04-8d48-bcd2ba861ce0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
