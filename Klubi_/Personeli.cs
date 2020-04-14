ac2625d01f2d44b9bd4a0fcb22e7d7d5d185662a
Arnis Hasani <arnis.hasani@riinvest.net>
2020-04-14 23:21:10 +0200

    shtimi i klases Personeli te DAL

---
+++ Klubi_/Personeli.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klubi_
+{
+    class Personeli
+    {
+        public int ID { get; set; }
+        public string Emri { get; set; }
+    }
+}
