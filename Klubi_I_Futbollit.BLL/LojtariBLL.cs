a3049c0a01d4660a63adbf68f70004637bab7628
Arnis Hasani <arnis.hasani@riinvest.net>
2020-04-23 00:05:02 +0200

    Krijimi i klases LojtariBLL

---
+++ Klubi_I_Futbollit.BLL/LojtariBLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klubi_I_Futbollit.BLL
+{
+    class LojtariBLL
+    {
+    }
+}
